f6e30ef00ad92a2f8bd576bfd76909399ca4a0c1
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2018-09-17 15:16:36 +0530

    Add files via upload

---
+++ basic hellow pg.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class hellow
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("hellow,word");
+	}
+}
