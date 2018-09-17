b5f3b1b12bc9752f963f44825ab677bf9fc97c5b
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2018-09-17 15:13:06 +0530

    Add files via upload

---
+++ if or else.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      
+     int Daysintheater=70;
+      if(Daysintheater<50)
+      {
+      System.out.println(" Hit:");
+      }
+      else
+      {
+       System.out.println(" Flop:");
+        }
+		
+	}
+}
