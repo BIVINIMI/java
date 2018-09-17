9a151bbff95fc1927653cd485e5415f9280ef5aa
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2018-09-17 15:22:57 +0530

    Add files via upload

---
+++ ellipse.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ellipse
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+      double a,z1=8,z2=6,z3=5,v,p;
+      a=3.14*z1*z2;
+      v=(4.0/3.0)*3.14*z1*z2*z3;
+      p=2*3.14*(Math.pow(z1,2)+Math.pow(z2,2))/2));
+      System.out.println("area of ellipse="+a);
+      system.out.println("\nvolume of ellipse="+v);
+      system.out.println("\nperimeter of ellipse="+p);
+      ;
+		
+	}
+}
+       
