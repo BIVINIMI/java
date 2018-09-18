9274db6035021b5c635257fb7718069d79d9a49b
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2018-09-18 14:30:57 +0530

    Add files via upload

---
+++ variable operation.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GUVI
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q;
+      a=60;
+      b=70;
+      c=a+b;     
+      d=a-b;
+      e=a*b;
+      f=b/a;
+      g=b%a;
+      h=b++;
+      i=b--;
+      j=a&b;
+      k=a|b;
+      l=a^b;
+      m=~a;
+      n=a<<2;
+      o=a>>>2;
+      p=a|b;
+      q=(a&b);
+
+      System.out.println("addition of the numbers:"+c);
+      System.out.println("subtraction of the numbers:"+d);
+      System.out.println("multiplication of the numbers:"+e);
+      System.out.println("numbers:"+f);
+      System.out.println("  numbers:"+g);
+      System.out.println("  numbers:"+h);
+      System.out.println("  numbers:"+i);
+      System.out.println(" numbers:"+j);
+      System.out.println(" numbers:"+k);
+      System.out.println("numbers:"+l);
+      System.out.println(" numbers:"+m);
+      System.out.println(" numbers:"+n);
+      System.out.println(" numbers:"+o);
+      System.out.println(" numbers:"+p);
+      System.out.println(" numbers:"+q);
+      
+      
+    
+	}
+}
