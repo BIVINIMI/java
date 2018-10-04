6a377fe620d14636e4ff90b209e2c77595987359
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2018-10-04 12:35:33 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Vowel {
+
+    public static void main(String[] args) {
+
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}
