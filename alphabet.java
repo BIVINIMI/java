c39403b6527eaaa5b818ce4795ceed4392f9b639
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2018-10-05 11:50:19 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Main {
+
+    public static void main(String[] args) {
+
+        char c = 'h';
+
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
