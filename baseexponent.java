65b5d065624858db1210c3f4a369c0044169fa40
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2019-01-13 14:06:14 +0530

    Add files via upload

---
+++ baseexponent.java
@@ -0,0 +1,24 @@
+public class base{
+
+    
+ public static void main(String []args){
+     
+int base =4, Exponent=5;
+    
+ long Result =1;
+    
+ while (Exponent !=0)
+    
+ {
+     
+Result*=base;
+
+ --Exponent;
+    
+ }  
+    
+ System.out.println("Answer=" + Result);
+    
+ }
+
+}
