dd675667d5177e1b64c51624a14bb44786b24ea7
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2019-01-14 09:02:34 +0530

    Add files via upload

---
+++ integernumber.java
@@ -0,0 +1,8 @@
+import.java.until.*;
+class NumberDigits
+{
+public statiC void main(Strings[]args){
+int count = 0,num =24686;
+for(num! =10, num/=10,++count);}
+System.out.println("number of digits"+ counts)
+}
