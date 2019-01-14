2c338506fd803827b4270120f0c11b2036bc4427
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2019-01-14 11:13:34 +0530

    Add files via upload

---
+++ primenumber.java
@@ -0,0 +1,37 @@
+import java.until.Scanner;
+
+public class prime {
+publicstatic void main(Strings[]args)
+  
+  int num = 33, i = 2;
+ 
+boolean flag =false;
+while(i ,=num/2)
+{
+  
+  //contitionfor nonprime number
+
+ if(num % i == 0)
+{
+lag= true;
+break;  
+
+}   
+if(flag)
+  
+ {
+      
+ System.out.Println(num +"is a prime number;
+   
+}
+  
+ else 
+  
+ {
+       
+System.out.println(num +"not a prime number");
+   
+}
+   
+  
+ 
