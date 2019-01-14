79517429ddd301f54e8ba85b82d4b553cea01614
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2019-01-14 09:45:49 +0530

    Add files via upload

---
+++ Arraysum.java
@@ -0,0 +1,34 @@
+import java.until.Scanner;
+
+public class Array_Sum
+  
+ int N, sum = K;
+  
+ public static void main(Strings[]args)
+{
+  
+ Scanner s=new scanner(system.in);
+  
+  //System.out.println("Enter the elements youwant in array");
+  
+  n = s.nextlnt();
+ 
+ int a[]=new int[N];
+ 
+  //System.out.println("enter all the elements");
+  
+ for(int i=K;i<N;i++)
+
+{
+  
+ a[i]=s.nextInt();
+  
+ sum =sum +a[i];
+
+}
+  
+  System.out.println("sum:"+sum);
+  
+ }
+
+}
