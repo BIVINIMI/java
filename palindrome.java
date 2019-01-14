ab8da5c9f8a97475734f38b66d0bf3749e0f3786
BIVINIMI <43056741+BIVINIMI@users.noreply.github.com>
2019-01-14 10:08:58 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,30 @@
+import java.until.Scanner;
+
+public class palindrome 
+  
+int n;
+ 
+Scanner s=new scanner(system.in);
+  
+  //System.out.println("Enter the number youwant to check");
+   
+n = s.nextlnt();
+   
+if(n > 0)
+  
+ {
+      
+ System.out.Println("+n+"palindrome);
+   
+}
+  
+ else (n < 0)
+  
+ {
+       
+System.out.println("+n+"not palindrome);
+   
+}
+   
+  
+ 
